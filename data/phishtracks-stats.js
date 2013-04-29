ba090d8c5b49bf6759a615f3126b497b2e9ac499
Alex Bird <alexebird@gmail.com>
2013-04-29 00:48:47 -0500

    Figured out how to access show info.

--- data/phishtracks-stats.js
+++ data/phishtracks-stats.js
@@ -1,19 +1,37 @@
-// PhishTracks Stats var
+// TODO
+// X get show info
+// - hook doesnt work for first song when visiting a song url. createSound is 
+//   called before the hook is installed.
+
+var wrappedWindow = content.wrappedJSObject;
+
+// PhishTracks Stats (PTS) var
 var PTS = {
-	soundManager: null,
-	player: null
+	soundManager: wrappedWindow.soundManager,
+	player: wrappedWindow.App.player
 };
 
-PTS.player = content.wrappedJSObject.App.player;
-PTS.soundManager = content.wrappedJSObject.soundManager;
+PTS.initOnFinishHook = function() {
+	alert("installing hook");
+	var oldCreateSound = this.soundManager.createSound;
+	var _this = this;
+	this.soundManager.createSound = function(options) {
+		alert('createSound hook called');
+		//insert callback to add-on in onFinish event
 
-var oldCreateSound = PTS.soundManager.createSound;
-PTS.soundManager.createSound = function(options) {
-	//insert callback to add-on in onFinish event
-	options.onfinish = function() {
-		PTS.player.playNext();
-		alert("INJECTION");
-	};
+		var callbackName = 'onfinish';
+		var oldCallback = options[callbackName];
+		options[callbackName] = function() {
+			var currTrack = _this.player.get("currentTrack");
+			var currShow = currTrack.collection.show;
+			self.port.emit("trackFinished", { track: currTrack,
+											  show: currShow });
+			oldCallback();
+			alert("onfinish callback called");
+		};
 
-	return oldCreateSound.call(PTS.soundManager, options);
-} 
+		return oldCreateSound.call(_this.soundManager, options);
+	} 
+}
+
+PTS.initOnFinishHook();
