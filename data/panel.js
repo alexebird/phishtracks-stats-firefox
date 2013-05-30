218116f385e1adc513edfd814852542644f2abdd
Alex Bird <alexebird@gmail.com>
2013-05-30 19:44:36 -0400

    Got stats showing.

--- data/panel.js
+++ data/panel.js
@@ -1,11 +1,13 @@
 // Content script for login panel (sign_in.html)
 
 $("#new_user").submit(function() {
-	console.log("login form submitted");
+	console.log("panel: login form submitted");
 	var userLogin = { "user_login": { 
 					       "login":    $("#user_login").val(),
 					       "password": $("#user_password").val() }};
-	self.port.emit("submit", userLogin);
+    // $("#user_login").val("");
+    // $("#user_password").val("");
+	self.port.emit("submit_login", userLogin);
 	return false;
 });
 
@@ -16,25 +18,64 @@ self.port.on("sign_in_response", function(response) {
 
 	}
 	else {
-		// change color for error feedback or something
+		$("#login_feedback").css("color", "red");
 	}
 });
 
 
 // stats.html
 
-$("#sign-out-button").click(function(){
+$("#sign_out_button").click(function(){
 	self.port.emit("sign_out");
 });
 
-$("#refresh-stats").click(function(){
+$("#refresh_stats_button").click(function(){
 	self.port.emit("manual_stats_refresh");
 });
 
-self.port.on("overall_stats_update", function(stats) {
-	$("#overall-stats").html(JSON.stringify(stats));
+function renderCalculationRow(name, value) {
+	var newRow = $('<tr class="calculations_row"><td class="calculation_name"></td><td class="calculation_value"></td></tr>');
+	$(".calculation_name", newRow).html(name);
+	$(".calculation_value", newRow).html(value);
+	return newRow;
+}
+
+function renderTopTracksTable(top_tracks) {
+	var newBody = $("<tbody>");
+
+	$.each(top_tracks, function(i, track) {	
+		var newRow = $('<tr class="most_played_row"><td class="most_played_ranking"></td><td class="most_played_name"></td><td class="most_played_play_count"></td></tr>');
+		$(".most_played_ranking", newRow).html(i + 1);
+		var trackLink = $('<a class="open_tab_link">');
+		trackLink.html(track.title);
+		var url = 'http://www.phishtracks.com/' + track.url;
+		trackLink.attr("data-url", url);
+		$(".most_played_name", newRow).html(trackLink);
+		$(".most_played_play_count", newRow).html(track.play_count);
+		newBody.append(newRow);
+	});
+
+	return newBody;
+}
+
+function installClickEventHandlers() {
+	$(".open_tab_link").click(function(eventObject) {
+		self.port.emit("open_tab_link_clicked", $(eventObject.target).attr("data-url"));
+	});
+}
+
+self.port.on("overall_stats_updated", function(stats) {
+	$("#overall_stats_most_played").empty().html(renderTopTracksTable(stats.top_tracks));
+	$("#overall_stats_calculations").empty().append(renderCalculationRow("Tracks played", stats.tracks_played));
+	$("#overall_stats_calculations").append(renderCalculationRow("Total time", stats.total_time));
+	installClickEventHandlers();
+	// console.log($("#overall_stats").html());
 });
 
-self.port.on("user_stats_update", function(stats) {
-	$("#user-stats").html(JSON.stringify(stats));
+self.port.on("user_stats_updated", function(stats) {
+	$("#user_stats_most_played").empty().html(renderTopTracksTable(stats.top_tracks));
+	$("#user_stats_calculations").empty().append(renderCalculationRow("Tracks played", stats.tracks_played));
+	$("#user_stats_calculations").append(renderCalculationRow("Total time", stats.total_time));
+	$("#user_stats_calculations").append(renderCalculationRow("Progress", stats.catalog_percentage));
+	installClickEventHandlers();
 });
--- data/pts.js
+++ data/pts.js
@@ -1,6 +1,6 @@
-var wrappedWindow = content.wrappedJSObject;
+var wrappedWindow = content.wrappedJSObject;  // Escape FF's sandbox for add-ons
 
-// PhishTracks Stats (PTS) var
+// PhishTracks Stats (PTS)
 var PTS = {
 	soundManager: wrappedWindow.soundManager,
 	player: wrappedWindow.App.player
@@ -8,31 +8,30 @@ var PTS = {
 
 PTS.wrapOnFinishCallback = function(options) {
 	if (options.__pts_onfinishWrapped == true) {
-		console.log("onfinish already wrapped")
+		console.log("pts: onfinish already wrapped")
 		return;
 	}
 
-	console.log("wrapping onfinish for '" + options.id + "'")
+	console.log("pts: wrapping onfinish for '" + options.id + "'")
 	var oldOnFinish = options.onfinish;
 
 	options.onfinish = function(callback) {
 		var currTrack = PTS.player.get("currentTrack");
-		var currShow = currTrack.collection.show;
-		self.port.emit("track_finished", { playedTrack: currTrack,
-										  show: currShow });
+		// var currShow = currTrack.collection.show;
+		self.port.emit("track_finished", currTrack.id );
 		oldOnFinish();
-		console.log("onfinish callback wrapper");
+		console.log("pts: onfinish callback wrapper");
 	}
 
 	options.__pts_onfinishWrapped = true;
 }
 
 PTS.wrapCreateSound = function() {
-	console.log("wrapping createSound");
+	console.log("pts: wrapping createSound");
 	var oldCreateSound = PTS.soundManager.createSound;
 
 	PTS.soundManager.createSound = function(options) {
-		console.log("createSound wrapper");
+		console.log("pts: createSound wrapper");
 		PTS.wrapOnFinishCallback(options);
 		return oldCreateSound.call(PTS.soundManager, options);
 	} 
@@ -45,7 +44,7 @@ PTS.hookAlreadyPlayingTrack = function() {
 		var sound = sounds[key];
 		
 		if (sound.playState == 1) {  // 1 indicates playing or buffering
-			console.log("sound is playing");
+			console.log("pts: sound is playing");
 			PTS.wrapOnFinishCallback(sound._iO);
 		}
 	}
