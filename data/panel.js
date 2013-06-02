bed6fc31f2115668167da3faad8f7ed634ae8511
Alex Bird <alexebird@gmail.com>
2013-06-01 21:33:11 -0400

    Added user history api call and UI tweaks.

--- data/panel.js
+++ data/panel.js
@@ -33,49 +33,69 @@ $("#refresh_stats_button").click(function(){
 	self.port.emit("manual_stats_refresh");
 });
 
-function renderCalculationRow(name, value) {
-	var newRow = $('<tr class="calculations_row"><td class="calculation_name"></td><td class="calculation_value"></td></tr>');
-	$(".calculation_name", newRow).html(name);
-	$(".calculation_value", newRow).html(value);
-	return newRow;
-}
+// function renderCalculationRow(name, value) {
+// 	var newRow = $('<tr class="calculations_row"><td class="calculation_name"></td><td class="calculation_value"></td></tr>');
+// 	$(".calculation_name", newRow).html(name);
+// 	$(".calculation_value", newRow).html(value);
+// 	return newRow;
+// }
 
-function renderTopTracksTable(top_tracks) {
-	var newBody = $("<tbody>");
+// function renderTopTracksTable(top_tracks) {
+// 	var newBody = $("<tbody>");
 
-	$.each(top_tracks, function(i, track) {	
-		var newRow = $('<tr class="most_played_row"><td class="most_played_ranking"></td><td class="most_played_name"></td><td class="most_played_play_count"></td></tr>');
-		$(".most_played_ranking", newRow).html(i + 1);
-		var trackLink = $('<a class="open_tab_link">');
-		trackLink.html(track.title);
-		var url = 'http://www.phishtracks.com/' + track.url;
-		trackLink.attr("data-url", url);
-		$(".most_played_name", newRow).html(trackLink);
-		$(".most_played_play_count", newRow).html(track.play_count);
-		newBody.append(newRow);
-	});
+// 	$.each(top_tracks, function(i, track) {	
+// 		var newRow = $('<tr class="most_played_row"><td class="most_played_ranking"></td><td class="most_played_name"></td><td class="most_played_play_count"></td></tr>');
+// 		$(".most_played_ranking", newRow).html(i + 1);
+// 		var trackLink = $('<a class="open_tab_link">');
+// 		trackLink.html(track.title);
+// 		var url = 'http://www.phishtracks.com/' + track.url;
+// 		trackLink.attr("data-url", url);
+// 		$(".most_played_name", newRow).html(trackLink);
+// 		$(".most_played_play_count", newRow).html(track.play_count);
+// 		newBody.append(newRow);
+// 	});
 
-	return newBody;
-}
+// 	return newBody;
+// }
 
-function installClickEventHandlers() {
-	$(".open_tab_link").click(function(eventObject) {
+function installOpenTabLinkHandlers() {
+	$(".open-tab-link").click(function(eventObject) {
 		self.port.emit("open_tab_link_clicked", $(eventObject.target).attr("data-url"));
 	});
 }
 
+// installOpenTabLinkHandlers();  // TODO update classes
+
 self.port.on("overall_stats_updated", function(stats) {
-	$("#overall_stats_most_played").empty().html(renderTopTracksTable(stats.top_tracks));
-	$("#overall_stats_calculations").empty().append(renderCalculationRow("Tracks played", stats.tracks_played));
-	$("#overall_stats_calculations").append(renderCalculationRow("Total time", stats.total_time));
-	installClickEventHandlers();
-	// console.log($("#overall_stats").html());
+	$("#overall-tracks-played-value").html(stats.tracks_played);
+	$("#overall-total-time-value").html(stats.total_time);
+
+	var rows = $("#overall-top-tracks tr");
+	$.each(stats.top_tracks, function(i, track) {
+		var row = $(rows[i]);
+		$(".track-ranking", row).html(i + 1);
+		$("a.track-title", row).html(track.title);
+		$("a.track-title", row).attr("data-url", track.url);
+		$("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
+		$("a.show-date", row).attr("data-url", track.show_info.url);
+		$(".play-count", row).html(track.play_count + " plays");
+	});
 });
 
 self.port.on("user_stats_updated", function(stats) {
-	$("#user_stats_most_played").empty().html(renderTopTracksTable(stats.top_tracks));
-	$("#user_stats_calculations").empty().append(renderCalculationRow("Tracks played", stats.tracks_played));
-	$("#user_stats_calculations").append(renderCalculationRow("Total time", stats.total_time));
-	$("#user_stats_calculations").append(renderCalculationRow("Progress", stats.catalog_percentage));
-	installClickEventHandlers();
+	$("#user-tracks-played-value").html(stats.tracks_played);
+	$("#user-total-time-value").html(stats.total_time);
+	$("#user-catalog-progress-value").html(stats.catalog_progress.split(" ").join("<br/>"));
+});
+
+self.port.on("user_history_updated", function(history) {
+	var rows = $("#user-history tr");
+	$.each(history, function(i, track) {
+		var row = $(rows[i]);
+		$("a.track-title", row).html(track.title);
+		$("a.track-title", row).attr("data-url", track.url);
+		$("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
+		$("a.show-date", row).attr("data-url", track.show_info.url);
+		$(".play-count", row).html(track.play_count + " plays");
+	});
 });
