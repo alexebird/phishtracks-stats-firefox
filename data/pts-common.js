4054b020da17c8adba7bd574d853e0a83f78f12b
Alex Bird <alexebird@gmail.com>
2013-06-13 00:54:09 -0400

    Styled add on panels. Working out kinks and what a good UI flow should be.

---
+++ data/pts-common.js
@@ -0,0 +1,5 @@
+function installOpenTabLinkHandlers() {
+	$(".open-tab-link").click(function(eventObject) {
+		self.port.emit("open_tab_link_clicked", $(eventObject.target).attr("data-url"));
+	});
+}
