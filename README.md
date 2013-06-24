951d47c5bc71e323dd041d2324a4b6254ff70f97
Alex Bird <alexebird@gmail.com>
2013-06-23 21:47:02 -0400

    Fixed some things revealed by lint.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ PhishTracks Stats
 ===
 
 A Firefox Add-on that keeps track of your PhishTracks.com listening stats.
+
+cfx run --profiledir=fxprofile --binary-args '-url "http://phishtracks.com/"'
+cfx run --profiledir=fxprofile --binary-args '-url "http://phishtracks.com/shows/1994-06-17/simple--3"'
--- data/sign-in-panel.js
+++ data/sign-in-panel.js
@@ -4,30 +4,30 @@
 /************** Sign In *************/
 
 $("#new-user").submit(function() {
-	console.log("SIGN-IN PANEL: login form submitted");
-	var userLogin = { "user_login": { 
-					       "login":    $("#user-login").val(),
-					       "password": $("#user-password").val() }};
-	self.port.emit("login_submitted", userLogin);
-	return false;
+    console.log("SIGN-IN PANEL: login form submitted");
+    var userLogin = { "user_login": {
+                                        "login":    $("#user-login").val(),
+                           "password": $("#user-password").val() }};
+    self.port.emit("login_submitted", userLogin);
+    return false;
 });
 
 
 // Event handling
 
 self.port.on("sign_in_response", function(response) {
-	if (!response.success) {
-		// console.log("DEBUG: sign in success");
-		// $("#sign-in-alert").removeClass("alert-error").addClass("alert-success");
-		// $("#sign-in-alert .alert-text").html("Sign in successful.");
-		// $("#sign-in-alert").show();
-	// }
-	// else {
-		// console.log("DEBUG: sign in failed");
-		// $("#sign-in-alert").removeClass("alert-success").addClass("alert-error");
-		$("#sign-in-alert .alert-text").html("<b>Error!</b> " + response.message);
-		$("#sign-in-alert").show();
-	}
+    if (!response.success) {
+        // console.log("DEBUG: sign in success");
+        // $("#sign-in-alert").removeClass("alert-error").addClass("alert-success");
+        // $("#sign-in-alert .alert-text").html("Sign in successful.");
+        // $("#sign-in-alert").show();
+    // }
+    // else {
+        // console.log("DEBUG: sign in failed");
+        // $("#sign-in-alert").removeClass("alert-success").addClass("alert-error");
+        $("#sign-in-alert .alert-text").html("<b>Error!</b> " + response.message);
+        $("#sign-in-alert").show();
+    }
 });
 
 self.port.on("reset_login_form", function() {
@@ -40,45 +40,45 @@ self.port.on("reset_login_form", function() {
 /************** Stats *************/
 
 $("#sign-out-button").click(function(){
-	self.port.emit("sign_out");
+    self.port.emit("sign_out");
 });
 
 $("#refresh-button").click(function(){
-	self.port.emit("manual_refresh");
+    self.port.emit("manual_refresh");
 });
 
 self.port.on("overall_stats_updated", function(stats) {
-	$("#overall-tracks-played-value").html(stats.tracks_played);
-	$("#overall-total-time-value").html(stats.total_time);
+    $("#overall-tracks-played-value").html(stats.tracks_played);
+    $("#overall-total-time-value").html(stats.total_time);
 
-	var rows = $("#overall-top-tracks tr");
-	$.each(stats.top_tracks, function(i, track) {
-		var row = $(rows[i]);
-		$(".track-ranking", row).html(i + 1);
-		$("a.track-title", row).html(track.title);
-		$("a.track-title", row).attr("data-url", track.url);
-		$("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
-		$("a.show-date", row).attr("data-url", track.show_info.url);
-		$(".play-count", row).html(track.play_count + " plays");
-	});
+    var rows = $("#overall-top-tracks tr");
+    $.each(stats.top_tracks, function(i, track) {
+        var row = $(rows[i]);
+        $(".track-ranking", row).html(i + 1);
+        $("a.track-title", row).html(track.title);
+        $("a.track-title", row).attr("data-url", track.url);
+        $("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
+        $("a.show-date", row).attr("data-url", track.show_info.url);
+        $(".play-count", row).html(track.play_count + " plays");
+    });
 });
 
 self.port.on("user_stats_updated", function(stats) {
-	$("#user-tracks-played-value").html(stats.tracks_played);
-	$("#user-total-time-value").html(stats.total_time);
-	$("#user-catalog-progress-value").html(stats.catalog_progress.split(" ").join("<br/>"));
+    $("#user-tracks-played-value").html(stats.tracks_played);
+    $("#user-total-time-value").html(stats.total_time);
+    $("#user-catalog-progress-value").html(stats.catalog_progress.split(" ").join("<br/>"));
 });
 
 self.port.on("user_history_updated", function(history) {
-	var rows = $("#user-history tr");
-	$.each(history, function(i, track) {
-		var row = $(rows[i]);
-		$("a.track-title", row).html(track.title);
-		$("a.track-title", row).attr("data-url", track.url);
-		$("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
-		$("a.show-date", row).attr("data-url", track.show_info.url);
-		$(".time-since-played", row).html(track.time_since_played + ' ago');
-	});
+    var rows = $("#user-history tr");
+    $.each(history, function(i, track) {
+        var row = $(rows[i]);
+        $("a.track-title", row).html(track.title);
+        $("a.track-title", row).attr("data-url", track.url);
+        $("a.show-date", row).html(track.show_info.show_date + ' - ' + track.show_info.location);
+        $("a.show-date", row).attr("data-url", track.show_info.url);
+        $(".time-since-played", row).html(track.time_since_played + ' ago');
+    });
 });
 
 installOpenTabLinkHandlers();
