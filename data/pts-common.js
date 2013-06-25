597f507e4ec70d045cbf88af3df37254aaef001a
Alex Bird <alexebird@gmail.com>
2013-06-24 22:02:44 -0400

    Fixed logo and other various styling issues. Added retina.js support.

--- data/pts-common.js
+++ data/pts-common.js
@@ -2,4 +2,8 @@ function installOpenTabLinkHandlers() {
 	$(".open-tab-link").click(function(eventObject) {
 		self.port.emit("open_tab_link_clicked", $(eventObject.target).attr("data-url"));
 	});
+
+  $("#pts-logo").click(function(eventObject) {
+    self.port.emit("open_tab_link_clicked", "{pts}/");
+  });
 }
